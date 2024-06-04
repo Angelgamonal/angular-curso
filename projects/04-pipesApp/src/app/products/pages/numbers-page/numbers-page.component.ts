f96ba36aae79ad3bc4c39aa8dac57aaddb43a952
angelgamonal <angelgamonal.t@gmail.com>
2024-06-04 16:37:06 -0500

    add numbers pipe

--- projects/04-pipesApp/src/app/products/pages/numbers-page/numbers-page.component.ts
+++ projects/04-pipesApp/src/app/products/pages/numbers-page/numbers-page.component.ts
@@ -3,8 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-numbers-page',
   templateUrl: './numbers-page.component.html',
-  styleUrl: './numbers-page.component.css'
+  styleUrl: './numbers-page.component.css',
 })
 export class NumbersPageComponent {
-
+  totalSells = 25831390.3901;
+  percent = 0.4856;
 }
