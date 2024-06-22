f592039fe1866a2a46eefcc22d6655cdebdc8252
angelgamonal <angelgamonal.t@gmail.com>
2024-06-22 03:33:14 -0500

    add login and register template

--- projects/05-heroes-app/src/app/auth/auth.module.ts
+++ projects/05-heroes-app/src/app/auth/auth.module.ts
@@ -5,9 +5,10 @@ import { AuthRoutingModule } from './auth-routing.module';
 import { LayoutComponent } from './layout/layout.component';
 import { LoginPageComponent } from './page/login-page/login-page.component';
 import { RegisterPageComponent } from './page/register-page/register-page.component';
+import { MaterialModule } from '../material/material.module';
 
 @NgModule({
   declarations: [LayoutComponent, LoginPageComponent, RegisterPageComponent],
-  imports: [CommonModule, AuthRoutingModule],
+  imports: [CommonModule, AuthRoutingModule, MaterialModule],
 })
 export class AuthModule {}
