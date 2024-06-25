411bf3e9d1b3fd6e793897b9f7279e5852c9a345
angelgamonal <angelgamonal.t@gmail.com>
2024-06-25 01:30:53 -0500

    add sibebar material

--- projects/05-heroes-app/src/app/heroes/heroes.module.ts
+++ projects/05-heroes-app/src/app/heroes/heroes.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HeroesRoutingModule } from './heroes-routing.module';
+import { MaterialModule } from '../material/material.module';
+
 import { HeroPageComponent } from './page/hero-page/hero-page.component';
 import { LayoutComponent } from './layout/layout.component';
 import { ListPageComponent } from './page/list-page/list-page.component';
@@ -9,7 +11,13 @@ import { NewPageComponent } from './page/new-page/new-page.component';
 import { SearchPageComponent } from './page/search-page/search-page.component';
 
 @NgModule({
-  declarations: [HeroPageComponent, LayoutComponent, ListPageComponent, NewPageComponent, SearchPageComponent],
-  imports: [CommonModule, HeroesRoutingModule],
+  declarations: [
+    HeroPageComponent,
+    LayoutComponent,
+    ListPageComponent,
+    NewPageComponent,
+    SearchPageComponent,
+  ],
+  imports: [CommonModule, HeroesRoutingModule, MaterialModule],
 })
 export class HeroesModule {}
