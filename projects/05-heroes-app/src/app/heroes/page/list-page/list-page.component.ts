5d23f02dd3106b845a2841897aecfd7ae2ac371c
angelgamonal <angelgamonal.t@gmail.com>
2024-07-01 01:25:51 -0500

    add handle error

--- projects/05-heroes-app/src/app/heroes/page/list-page/list-page.component.ts
+++ projects/05-heroes-app/src/app/heroes/page/list-page/list-page.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 import { Hero } from '../../interfaces/hero.interface';
 import { HeroesService } from '../../services/heroes.service';
+import { catchError, of } from 'rxjs';
 
 @Component({
   selector: 'app-list-page',
@@ -10,12 +11,19 @@ import { HeroesService } from '../../services/heroes.service';
 })
 export class ListPageComponent implements OnInit {
   public heroes: Hero[] = [];
+  public errorHeroe: null | string = null;
 
   constructor(private heroesService: HeroesService) {}
 
   ngOnInit(): void {
     this.heroesService
       .getHeroes()
+      .pipe(
+        catchError(() => {
+          this.errorHeroe = 'No se encontraron heroes, conecte su json server';
+          return of([]);
+        })
+      )
       .subscribe((heroes) => (this.heroes = heroes));
   }
 }
