ef2b73bedb59cdbfaa5ed8819ad375358c96b485
angelgamonal <angelgamonal.t@gmail.com>
2024-09-18 17:55:19 -0500

    add demo page

--- projects/05-heroes-app/src/app/heroes/page/new-page/new-page.component.ts
+++ projects/05-heroes-app/src/app/heroes/page/new-page/new-page.component.ts
@@ -3,8 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-new-page',
   templateUrl: './new-page.component.html',
-  styles: ``
+  styles: ``,
 })
 export class NewPageComponent {
-
+  public plublishers = [
+    { id: 'DC Comincs', value: 'DC - Comics' },
+    { id: 'Marvel Comincs', value: 'MARVEL - Comics' },
+  ];
 }
