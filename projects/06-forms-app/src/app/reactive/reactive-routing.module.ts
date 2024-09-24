185d4008075e2774462c69f288a22b1308ffa32d
angelgamonal <angelgamonal.t@gmail.com>
2024-09-24 16:41:38 -0500

    config routing

--- projects/06-forms-app/src/app/reactive/reactive-routing.module.ts
+++ projects/06-forms-app/src/app/reactive/reactive-routing.module.ts
@@ -1,10 +1,32 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BasicPageComponent } from './pages/basic-page/basic-page.component';
+import { DynamicPageComponent } from './pages/dynamic-page/dynamic-page.component';
+import { SwitchesPagesComponent } from './pages/switches-pages/switches-pages.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+      {
+        path: 'basic',
+        component: BasicPageComponent,
+      },
+      {
+        path: 'dynamic',
+        component: DynamicPageComponent,
+      },
+      {
+        path: 'switches',
+        component: SwitchesPagesComponent,
+      },
+      { path: '**', redirectTo: 'basic' },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class ReactiveRoutingModule { }
+export class ReactiveRoutingModule {}
