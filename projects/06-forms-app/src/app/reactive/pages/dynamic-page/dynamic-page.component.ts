9113b68e0dbc6239ad5faa79b67acfef2c320cf3
angelgamonal <angelgamonal.t@gmail.com>
2024-09-27 15:49:40 -0500

    dynamic form

--- projects/06-forms-app/src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ projects/06-forms-app/src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -1,10 +1,94 @@
 import { Component } from '@angular/core';
+import {
+  FormArray,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-dynamic-page',
   templateUrl: './dynamic-page.component.html',
-  styles: ``
+  styles: ``,
 })
 export class DynamicPageComponent {
+  // public myForm2 = new FormGroup({
+  //   favoriteGames: new FormArray([])
+  // })
 
+  public myForm: FormGroup = this.fb.group({
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    favoriteGames: this.fb.array([
+      ['dota2', Validators.required],
+      ['minecraft', Validators.required],
+    ]),
+  });
+
+  public newFavorite: FormControl = new FormControl('', [Validators.required]);
+
+  constructor(private fb: FormBuilder) {}
+
+  get favoriteGamesControls() {
+    return this.myForm.get('favoriteGames') as FormArray;
+  }
+
+  isValidField(field: string): boolean | null {
+    return (
+      this.myForm.controls[field].errors && this.myForm.controls[field].touched
+    );
+  }
+
+  isValidFieldInArray(formArray: FormArray, index: number) {
+    return (
+      formArray.controls[index].errors && formArray.controls[index].touched
+    );
+  }
+
+  getFieldError(field: string): string | null {
+    if (!this.myForm.controls[field]) return null;
+
+    const errors = this.myForm.controls[field].errors || {};
+
+    for (const key of Object.keys(errors)) {
+      switch (key) {
+        case 'required':
+          return 'este campo es requirido';
+
+        case 'minlength':
+          return `Mínimo ${errors['minlength'].requiredLength} caracteres`;
+      }
+    }
+
+    return 'hello word';
+  }
+
+  onDeleteFavorite(i: number) {
+    this.favoriteGamesControls.removeAt(i);
+  }
+
+  onAddToFavorite() {
+    if (this.newFavorite.invalid) {
+      this.newFavorite.markAllAsTouched();
+      return;
+    }
+
+    this.favoriteGamesControls.push(
+      this.fb.control(this.newFavorite.value, Validators.required)
+    );
+
+    this.newFavorite.reset('');
+  }
+
+  onSubmit(): void {
+    if (this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
+
+    console.log(this.myForm.value);
+
+    this.myForm.reset();
+    (this.myForm.controls['favoriteGames'] as FormArray) = this.fb.array([]);
+  }
 }
