636bef59f29d2bb6c229f48d6cea9c14c55bb98c
angelgamonal <angelgamonal.t@gmail.com>
2024-10-03 01:38:26 -0500

    switches form

--- projects/06-forms-app/src/app/reactive/pages/switches-pages/switches-pages.component.ts
+++ projects/06-forms-app/src/app/reactive/pages/switches-pages/switches-pages.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-switches-pages',
   templateUrl: './switches-pages.component.html',
-  styles: ``
+  styles: ``,
 })
 export class SwitchesPagesComponent {
+  public myForm: FormGroup = this.fb.group({
+    gender: ['M', Validators.required],
+    wantNotifications: [true, Validators.required],
+    termsAndConditions: [true, Validators.requiredTrue],
+  });
 
+  constructor(private fb: FormBuilder) {}
+
+  isValidField(field: string): boolean | null {
+    return (
+      this.myForm.controls[field].errors && this.myForm.controls[field].touched
+    );
+  }
+
+  onSubmit() {
+    if (this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
+    console.log(this.myForm.value);
+  }
 }
