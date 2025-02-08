03b1e82b7ef84524ec89b27fd065e18d5a0f665e
angelgamonal <angelgamonal.t@gmail.com>
2025-02-07 19:54:02 -0500

    add pages demo

---
+++ projects/maps-app/src/app/maps/components/mini-map/mini-map.component.ts
@@ -0,0 +1,7 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mini-map',
+  templateUrl: './mini-map.component.html',
+})
+export class MiniMapComponent {}
